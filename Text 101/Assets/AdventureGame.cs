993ec90d5fc3e63c1dd1331f6cd94aaed92cb344
jmb19812001 <39971095+jmb19812001@users.noreply.github.com>
2018-07-06 10:26:22 -0400

    First commit

---
+++ Text 101/Assets/AdventureGame.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    [SerializeField]
+    Text textComponent;
+    [SerializeField]
+    State StartingState;
+
+     State state;
+
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        state = StartingState;
+        textComponent.text = state.GetStateStory();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
